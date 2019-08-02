e7e109a62d8ae9f681ece4a485e04c055a359a0b
John Reyes <john.jreye@gmail.com>
2019-08-02 03:14:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson";
 
+var greet = "Hello,"
 var greetSpecialGuest = "greet" + 'specialGuest' + "!";
 
 var topic = "space";
